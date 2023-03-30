6f2190c85fbf2e0b6710c9a9f0a0ca052daade9d
vinnydiehl <vinny.diehl@gmail.com>
2023-03-30 11:35:32 -0400

    Animate level selector, make buttons clickable
    Made the level number pulse, made the buttons clickable and added pulse
    animations when hovered or activated.

--- app/tetris/render/main_menu.rb
+++ app/tetris/render/main_menu.rb
@@ -1,4 +1,32 @@
+ARROW_ALPHA = 100
+ARROW_BRIGHT_ALPHA = 200
+
 class TetrisGame
+  def main_menu_ui_init
+    @selector_buttons = [
+      {
+        path: "sprites/ui/chevron.png",
+        x: 140,
+        y: 140,
+        w: 35,
+        h: 50,
+        angle: 90,
+        a: ARROW_ALPHA,
+        value: 1
+      },
+      {
+        path: "sprites/ui/chevron.png",
+        x: 140,
+        y: 60,
+        w: 35,
+        h: 50,
+        angle: 270,
+        a: ARROW_ALPHA,
+        value: -1
+      }
+    ]
+  end
+
   def render_main_menu
     render_background
 
@@ -14,29 +42,11 @@ class TetrisGame
   end
 
   def render_level_selector
-    @args.outputs.sprites << [
-      {
-        path: "sprites/ui/chevron.png",
-        x: 140,
-        y: 140,
-        w: 35,
-        h: 50,
-        angle: 90,
-        a: 100
-      },
-      {
-        path: "sprites/ui/chevron.png",
-        x: 140,
-        y: 60,
-        w: 35,
-        h: 50,
-        angle: 270,
-        a: 100
-      }
-    ]
+    @args.outputs.sprites << @selector_buttons
 
     @args.outputs.labels << [
-      @starting_level.to_s.label(x: 159, y: 145, size: 8, alignment: :center),
+      @starting_level.to_s.label(x: 159, y: 145, size: 8, alignment: :center,
+        a: 77.5 * Math.sin(0.03 * (@args.state.tick_count - @selector_last_clicked) + 1.6) + 177.5),
       %w[choose level].span_vertically(x: 200, y: 150, spacing: 25)
     ]
   end
--- app/tetris/scenes/main_menu.rb
+++ app/tetris/scenes/main_menu.rb
@@ -6,6 +6,10 @@ class TetrisGame
 
     @selector_timeout = 0
     @selector_clicks_held = 0
+    @selector_last_clicked = @args.state.tick_count
+    @selector_last_direction = 0
+
+    main_menu_ui_init
   end
 
   def main_menu_tick
@@ -17,17 +21,45 @@ class TetrisGame
       set_scene :game
     end
 
-    if @args.inputs.up_down != 0
+    @selector_buttons.each do |button|
+      # We're going to be pulsing the arrows white on some actions, this makes it fade back.
+      # Doing this before setting bright alpha so there is no flickering
+      button[:a] -= 1 if button[:a] > ARROW_ALPHA
+
+      # Pulse button white on hover
+      button[:a] = ARROW_BRIGHT_ALPHA if @args.inputs.mouse.intersect_rect?(button)
+    end
+
+    # Pulse button white when using up/down inputs
+    if (@args.inputs.down && @starting_level > 0) || (@args.inputs.up && @starting_level < 15)
+      @selector_buttons.find { |b| b[:value] == @args.inputs.up_down }[:a] = ARROW_BRIGHT_ALPHA
+    end
+
+    button_clicked = @selector_buttons.find do |button|
+      @args.inputs.mouse.click&.point&.intersect_rect? button
+    end
+
+    if @args.inputs.up_down != 0 || button_clicked
       if @selector_timeout < 0
         # Hey, it sounds good here
         play_sound_effect "tetromino/move"
 
-        @starting_level = [@starting_level + @args.inputs.up_down, 1, 15].sort[1]
+        @selector_last_direction =
+          button_clicked ? button_clicked[:value] : @args.inputs.up_down
 
-        # Mechanism to make the selector click slowly for the first 3 clicks,
-        # then quickly the rest of the way if up/down is held
-        @selector_timeout = @selector_clicks_held < 3 ? 0.5.seconds : 0.1.seconds
-        @selector_clicks_held += 1
+        original = @starting_level
+        @starting_level = [@starting_level + @selector_last_direction, 1, 15].sort[1]
+
+        if @starting_level != original
+          # Mechanism to make the selector click slowly for the first 3 clicks,
+          # then quickly the rest of the way if up/down is held
+          @selector_timeout = @selector_clicks_held < 3 ? 0.5.seconds : 0.1.seconds
+          @selector_clicks_held += 1
+
+          # The alpha pulses in and out based on the tick count. This resets the
+          # counter so that when you change the level, it always starts at full alpha
+          @selector_last_clicked = @args.state.tick_count
+        end
       end
     else
       @selector_timeout = 0
